b2cb1a38516948262a95dc227315a9303aae290d
Matthias Busch <matthias.busch@ovgu.de>
2025-05-22 08:24:22 +0200

    wip server, add next todo

--- chatbot/chatbot_implementation.py
+++ chatbot/chatbot_implementation.py
@@ -1,31 +1,12 @@
-from chatbot import Chatbot
+from chatbot import animalbot
 
-class ChatbotImplementation(Chatbot):
+
+class ChatbotImplementation(animalbot):
 
     def __init__(self):
-        Chatbot.__init__(self)
+        animalbot.__init__(self)
 
     def get_prompt(self, messages, intent, session_id):
-
-        if intent["name"] == "climate_change_is_a_scientific_fact":
-            prompt = """This is a conversation between a user and a climate change denier. The climate change denier is very angry and insults the user. His believs are:
-
-    Climate change is a hoax.
-    All members of the green party are stupid.
-    All scientists are liars.
-
-"""
-            success = True
-        else:
-            prompt = """This is a conversation between a user and a climate change denier. His believs are:
-
-Climate change is a hoax.
-All members of the green party are stupid.
-All scientists are liars.
-
-"""
-            success = False
-
+        # TODO implement build_prompt method in animalbot
         final_prompt = self.build_prompt(prompt, messages)
         return final_prompt, success
-
