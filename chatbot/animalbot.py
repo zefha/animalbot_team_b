94b38c2ad2f9caa9911fc31d411bf267e2c4f06e
Hafez Bechnak <h.bechnak@campus.tu-berlin.de>
2025-06-26 11:35:07 +0200

    changed model sauerkraut

--- chatbot/animalbot.py
+++ chatbot/animalbot.py
@@ -71,7 +71,7 @@ class AnimalAgent:
         # Set custom base URL and API key directly in the ChatOpenAI initialization
         # Use the api_key that was determined outside of the class
         self.llm = ChatOpenAI(
-            model="meta-llama-3.1-8b-instruct",
+            model="llama-3.1-sauerkrautlm-70b-instruct",
             temperature=0.6,
             logprobs=True,
             openai_api_key=API_KEY,
