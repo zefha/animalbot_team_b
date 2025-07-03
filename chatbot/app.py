49582132e2974db8d3ad69f30615ff4882337954
Trujhi <ichhabekeingeld@gmail.com>
2025-07-03 11:57:25 +0200

    Update app.py
    Added Hinweis on the instructions

--- chatbot/app.py
+++ chatbot/app.py
@@ -86,6 +86,7 @@ st.title("🗣 Ask Me Anything")
 st.markdown("""
 Chatte mit Youssef, Irina, Rami, oder Duygu! 
 Sage einfach mit wem du sprechen möchtest, um den Charakter zu wählen.
+**Hinweis:** Bitte beachten Sie, dass dies eine Simulation ist und nicht als Ersatz für echte Gespräche oder Beratungen dienen soll. Es ist wichtig, sich an Fachleute zu wenden, wenn Sie persönliche oder sensible Themen besprechen möchten.
 """)
 
 
